5be791e93ff28ef822fa5c8fe25d0af7eca87bbf
Irem Şahinli <sahinliirem34@gmail.com>
2024-08-12 00:44:28 +0300

    Add project files.

---
+++ girilenSayilarUceBeseBolunmesi/Program.cs
@@ -0,0 +1,23 @@
+﻿int i = 1;
+int sayi, sayilar;
+
+
+Console.WriteLine("Sayı giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+while (i < 3)
+{
+    if (sayi % 3 == 0 && sayi % 5 == 0 && sayi % 15 == 0)
+    {
+        sayilar = sayi;
+        Console.WriteLine("Sayılar: " + sayilar + "şeklinde geliyor");
+       
+
+    }
+    i++;
+    Console.WriteLine("Tekrar sayı giriniz: ");
+    sayi = Convert.ToInt32(Console.ReadLine());
+
+
+}
+
